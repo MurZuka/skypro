8e6c80337a880c71afbcbcc5776e24f33f1ad821
MurZuka <murzuka@yahoo.com>
2025-06-11 15:38:19 +0300

    Коммит из ветки домашнего задания 2, уточнённое

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,31 @@ public class Main {
 
         System.out.println(dog + "\n" + cat + "\n" + paper);
 
-        dog += 4; cat +=4; paper += 4;
+        dog += 4;
+        cat +=4;
+        paper += 4;
 
         System.out.println(dog + "\n" + cat + "\n" + paper);
 
-        dog -= 3.5; cat -= 1.6; paper -= 7639;
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
 
         System.out.println(dog + "\n" + cat + "\n" + paper);
 
-        var friend = 19; System.out.println(friend);
-        friend += 2; System.out.println(friend);
-        friend /= 7; System.out.println(friend);
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
 
-        var frog = 3.5; System.out.println(frog);
-        frog *= 10; System.out.println(frog);
-        frog /= 3.5; System.out.println(frog);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
 
         var boxerWeight1 = 78.2;
         var boxerWeight2 = 82.7;
@@ -28,6 +38,7 @@ public class Main {
         System.out.println(boxerWeight1 - boxerWeight2);
 
         var weightsRemainder = boxerWeight2 % boxerWeight1;
+        System.out.println(weightsRemainder);
 
         var employeesNumber = 640;
         var hoursPerEmployee = 8;
