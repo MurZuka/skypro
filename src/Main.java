ecd0f66dc77a60775d13ae09da6b7eb2763ea795
MurZuka <murzuka@yahoo.com>
2025-06-11 15:35:33 +0300

    Коммит из ветки домашнего задания 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+
+        dog += 4; cat +=4; paper += 4;
+
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+
+        dog -= 3.5; cat -= 1.6; paper -= 7639;
+
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+
+        var friend = 19; System.out.println(friend);
+        friend += 2; System.out.println(friend);
+        friend /= 7; System.out.println(friend);
+
+        var frog = 3.5; System.out.println(frog);
+        frog *= 10; System.out.println(frog);
+        frog /= 3.5; System.out.println(frog);
+
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        System.out.println(boxerWeight1 + boxerWeight2);
+        System.out.println(boxerWeight1 - boxerWeight2);
+
+        var weightsRemainder = boxerWeight2 % boxerWeight1;
+
+        var employeesNumber = 640;
+        var hoursPerEmployee = 8;
+        System.out.println("Всего сотрудников в компании — " + employeesNumber / hoursPerEmployee + " человек");
+
+        employeesNumber += 94;
+        System.out.println("Если в компании работает " + employeesNumber + " человек, то всего " +
+                            employeesNumber / hoursPerEmployee + " часов работы может быть поделено между сотрудниками");
     }
 }
