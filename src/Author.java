22a1734352497da6d843ef78720eb0785c68fdd5
MurZuka <murzuka@yahoo.com>
2025-06-21 14:53:44 +0300

    Домашнее задание 13. Методы объектов

--- src/Author.java
+++ src/Author.java
@@ -14,4 +14,24 @@ public class Author {
     public String getSurName() {
         return surName;
     }
+
+    @Override
+    public String toString() {
+        return firstName + " " + surName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+
+        Author a2 = (Author) other;
+        return (firstName.equals(a2.firstName) && surName.equals(a2.surName));
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, surName);
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -24,4 +24,24 @@ public class Book {
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
     }
+
+    @Override
+    public String toString() {
+        return name + " " + author.toString() + " (" + releaseYear + ")";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+
+        Book b2 = (Book) other;
+        return (name.equals(b2.name) && author.equals(b2.author) && releaseYear == b2.releaseYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name, author, releaseYear);
+    }
 }
